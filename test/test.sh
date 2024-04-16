1e09a5c965cb41c86f7e86bbd2f3878685692d97
Jonatas Esteves <jntesteves@gmail.com>
2024-04-15 23:13:56 -0300

    Add test from preproc
    Copy the test from preproc with the exact same expected result. The test required corrections replacing many `echo` commands with `printf` because preproc truncates dangling white-space from the expansions, while template.sh behaves much better in this regard.
    
    It's best practice to use printf instead of echo in shell scripts anyway.

--- test/test.sh
+++ test/test.sh
@@ -7,3 +7,6 @@ template=../dist/template.sh
 
 $template -s test.context.sh -e ARG_VERSION=0.99.1-pre level_0.template >|level_0.out
 diff level_0.expected level_0.out
+
+$template preproc_test.template >preproc_test.out
+diff preproc_test.expected preproc_test.out
