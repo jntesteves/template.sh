ff94340c432dea0f29d21cc25c1a30416e727945
Jonatas Esteves <jntesteves@gmail.com>
2024-04-16 21:09:13 -0300

    Add test for a third level of recursion
    The test is huge now and covers all kinds of edge-cases, all interesting use-cases are covered, the test is pretty hopefully complete.

--- test/test.context.sh
+++ test/test.context.sh
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: Unlicense
 # shellcheck shell=sh disable=SC2034
-COMMAND=$0
+# $0 in a context file sourced with the -s option is the command template.sh itself.
+# Changed from $0 to hardcoding the value here to improve test reproducibility
+COMMAND=../dist/template.sh
 VERSION=0.1.0-pre
 TAG='tag'
 TWO_NEW_LINES='
