005cd681048b753fb2c047d405829ecd986392e5
Jonatas Esteves <jntesteves@gmail.com>
2024-11-09 08:49:08 -0300

    Update function assign_variable
    Guard against null variable name

--- template.sh
+++ template.sh
@@ -98,7 +98,7 @@ escape_single_quotes() { substitute_characters "$1" \' "'\\''"; }
 
 # Use indirection to dynamically assign a variable from argument NAME=VALUE
 assign_variable() {
-	case "${1%%=*}" in *[!_a-zA-Z0-9]* | [!_a-zA-Z]*) return 2 ;; esac
+	case "${1%%=*}" in '' | *[!_a-zA-Z0-9]* | [!_a-zA-Z]*) return 2 ;; esac
 	eval "${1%%=*}"='${1#*=}'
 }
 
